7c1a6b534bf78715bef6e6627c1f8a701d21d67f
FrankSiderio <frank_siderio@comcast.com>
2020-04-10 16:18:22 -0500

    updating api url in compile js

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,11 +7,6 @@
 import './bootstrap';
 import VueRouter from 'vue-router';
 import router from './routes';
-import socketio from 'socket.io-client';
-import VueSocketIO from 'vue-socket.io';
-
-// export const SocketInstance = socketio('http://127.0.0.1:3333');
-
 
 window.Vue = require('vue');
 
